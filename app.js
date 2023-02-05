19b7d2cd28c68dcd847aa5562a8a6212592c40ba
Tabrigos <guerrerapaolo@gmail.com>
2023-02-05 08:58:47 +0100

    client-server connection enstablished

--- app.js
+++ app.js
@@ -1,13 +1,11 @@
-
 const express = require('express');
-const app = express();
 const { createServer } = require("http");
 const { Server } = require("socket.io");
-//const serv = require('http').Server(app);
 
+const app = express();
 const httpServer = createServer(app);
 
-const io = new Server(httpServer, {});
+const io = new Server(httpServer);
 
 httpServer.listen(2000);
 
@@ -17,13 +15,10 @@ app.get('/', function(req,res) {
 
 app.use('/client', express.static(__dirname + '/client'));
 
-    console.log("Server started.");
+console.log("Server started.");
 
-io.on("connection", (socket) => {
-    console.log("client connected");
-  });
+io.on("connect", (socket) => {
 
-/*let io = require('socket.io')(serv, {});
-io.sockets.on('connection', function(socket){
-    console.log('socket connection');
-});*/
+    console.log("connected socket id: " + socket.id);
+
+});
---
+++ client/js/index.js
@@ -0,0 +1,6 @@
+
+const socket = io(`ws://localhost:2000`);
+
+socket.on('connect', () => {
+    console.log("connected")
+})
