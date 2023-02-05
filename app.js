8b915fafda9e486918526c37645e95ff9c51efec
Tabrigos <guerrerapaolo@gmail.com>
2023-02-05 07:50:54 +0100

    socket.io config

--- app.js
+++ app.js
@@ -9,3 +9,9 @@ app.get('/', function(req,res) {
 app.use('/client', express.static(__dirname + '/client'));
 
 serv.listen(2000);
+console.log("Server started.");
+
+let io = require('socket.io')(serv, {});
+io.sockets.on('connection', function(socket){
+    console.log('socket connection');
+});
